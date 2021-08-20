23bd4a9dafae9613e77731ea774d770a874d7c7c
Sooyeon Jo <supop1009@gmail.com>
2021-08-20 22:29:34 +0900

    Update initUI() for initializing UI components

--- app/src/main/java/com/example/soundlesscheck_in/ListenerFragment.java
+++ app/src/main/java/com/example/soundlesscheck_in/ListenerFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,20 +13,34 @@ import androidx.fragment.app.Fragment;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
-public class ListenerFragment extends Fragment {
-
-    // Variables related to getting current time
-    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+public class ListenerFragment extends Fragment{
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.KOREA);
     Date mDate;
     long mNow;
+    Button mBtnSetting;
+    Button mBtnGetInfo;
+    TextView mTextTime;
+    TextView mTextPhoneNumber;
+    TextView mTextViewCity;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_listener, container,false);
+    }
 
-        View v = inflater.inflate(R.layout.fragment_listener, container,false);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initUI(view);
+    }
 
-        return v;
-
+    private void initUI(View v){
+        mBtnSetting = v.findViewById(R.id.btnSetting_Listener);
+        mBtnGetInfo = v.findViewById(R.id.btnGetInfo);
+        mTextTime = v.findViewById(R.id.textViewTime_Listener);
+        mTextPhoneNumber = v.findViewById(R.id.textViewNum_Listener);
+        mTextViewCity = v.findViewById(R.id.textViewCity_Listener);
     }
 
     // return current time
