0189e25ba4a55f57227035d6b792d1ba7611572f
SEONG PIL JU <69073013+JuSeongPil@users.noreply.github.com>
2021-08-29 11:17:52 +0900

    Edit spinner

--- app/src/main/java/com/example/soundlesscheck_in/TutorialActivity.java
+++ app/src/main/java/com/example/soundlesscheck_in/TutorialActivity.java
@@ -58,24 +58,24 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
         mPhoneNumber.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
         mGetInfoBtn.setOnClickListener(this);
 
-        spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {//첫번째 spinner 클릭시 이벤트 발생입니다. setO 정도까지 치시면 자동완성됩니다. 뒤에도 마찬가지입니다.
+        spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {//제대로 자동완성하셨다면 이부분이 자동으로 만들어 질 것입니다. int i는 포지션이라 하여 제가 spinner에 몇번째걸 선택했는지 값이 들어갑니다. 필요하겠죠? ㅎㅎ
-                if (adspin1.getItem(i).equals("서울특별시")) {//spinner에 값을 가져와서 i 보이시나요 제가 클릭 한것이 서울인지 확인합니다.
-                    choice_do = "서울특별시";//버튼 클릭시 출력을 위해 값을 넣었습니다.
-                    adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_seoul, android.R.layout.simple_spinner_dropdown_item);//서울일 경우에 두번째 spinner에 값을 넣습니다. //그냥 this가 아닌 Main~~~인 이유는 그냥 this는 메인엑티비티인 경우만 가능합니다. //지금과 같이 다른 함수안이나 다른 클래스에서는 꼭 자신의 것을 넣어주어야 합니다.//혹시나 다른 class -> Public View밑에서 작업하시는 분은 view명.getContext()로 해주셔야 합니다.//예로 View rootView =~~ 선언하신 경우에는 rootView.getContext()써주셔야합니다. this가 아니라요.
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (adspin1.getItem(i).equals("서울특별시")) {
+                    choice_do = "서울특별시";
+                    adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_seoul, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    spin2.setAdapter(adspin2);//두번째 어댑터값을 두번째 spinner에 넣었습니다.
-                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {//저희는 두번째 선택된 값도 필요하니 이안에 두번째 spinner 선택 이벤트를 정의합니다.
+                    spin2.setAdapter(adspin2);
+                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                         @Override
                         public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                            choice_se = adspin2.getItem(i).toString();//두번째 선택된 값을 choice_se에 넣습니다.
+                            choice_se = adspin2.getItem(i).toString();
                         }
                         @Override
-                        public void onNothingSelected(AdapterView<?> adapterView) {//아무것도 선택안될시 부분입니다. 자동완성됩니다.
+                        public void onNothingSelected(AdapterView<?> adapterView) {
                         }
                     });
-                } else if (adspin1.getItem(i).equals("인천광역시")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                } else if (adspin1.getItem(i).equals("인천광역시")) {
                     choice_do = "인천광역시";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_incheon, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -90,7 +90,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                         }
                     });
                 }
-                else if (adspin1.getItem(i).equals("부산광역시")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("부산광역시")) {
                     choice_do = "부산광역시";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_busan, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -105,7 +105,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                         }
                     });
                 }
-                else if (adspin1.getItem(i).equals("대구광역시")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("대구광역시")) {
                     choice_do = "대구광역시";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_daegu, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -120,7 +120,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                         }
                     });
                 }
-                else if (adspin1.getItem(i).equals("대전광역시")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("대전광역시")) {
                     choice_do = "대전광역시";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_daejeon, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -135,7 +135,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                         }
                     });
                 }
-                else if (adspin1.getItem(i).equals("광주광역시")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("광주광역시")) {
                     choice_do = "광주광역시";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_gwangju, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -150,7 +150,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                         }
                     });
                 }
-                else if (adspin1.getItem(i).equals("울산광역시")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("울산광역시")) {
                     choice_do = "울산광역시";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_ulsan, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -165,7 +165,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                         }
                     });
                 }
-                else if (adspin1.getItem(i).equals("세종특별자치시")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("세종특별자치시")) {
                     choice_do = "세종특별자치시";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_sejong, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -180,7 +180,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                         }
                     });
                 }
-                else if (adspin1.getItem(i).equals("경기도")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("경기도")) {
                     choice_do = "경기도";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_gyeonggi, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -196,7 +196,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                         }
                     });
                 }
-                else if (adspin1.getItem(i).equals("강원도")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("강원도")) {
                     choice_do = "강원도";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_gangwon, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -212,7 +212,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                         }
                     });
                 }
-                else if (adspin1.getItem(i).equals("충청북도")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("충청북도")) {
                     choice_do = "충청북도";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_chung_buk, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -228,7 +228,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                         }
                     });
                 }
-                else if (adspin1.getItem(i).equals("충청남도")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("충청남도")) {
                     choice_do = "충청남도";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_chung_nam, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -244,7 +244,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                         }
                     });
                 }
-                else if (adspin1.getItem(i).equals("전라북도")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("전라북도")) {
                     choice_do = "전라북도";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_jeon_buk, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -261,7 +261,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                     });
                 }
 
-                else if (adspin1.getItem(i).equals("전라남도")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("전라남도")) {
                     choice_do = "전라남도";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_jeon_nam, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -278,7 +278,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                     });
                 }
 
-                else if (adspin1.getItem(i).equals("경상북도")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("경상북도")) {
                     choice_do = "경상북도";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_gyeong_buk, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -295,7 +295,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                     });
                 }
 
-                else if (adspin1.getItem(i).equals("경상남도")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("경상남도")) {
                     choice_do = "경상남도";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_gyeong_nam, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -312,7 +312,7 @@ public class TutorialActivity extends AppCompatActivity implements View.OnClickL
                     });
                 }
 
-                else if (adspin1.getItem(i).equals("제주특별자치도")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                else if (adspin1.getItem(i).equals("제주특별자치도")) {
                     choice_do = "제주특별자치도";
                     adspin2 = ArrayAdapter.createFromResource(TutorialActivity.this, R.array.spinner_do_jeju, android.R.layout.simple_spinner_dropdown_item);
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
