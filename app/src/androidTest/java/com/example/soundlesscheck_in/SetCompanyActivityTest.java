797c0646bf213c19964c9253f7caa45250fc2495
aiclaudev <88221233+aiclaudev@users.noreply.github.com>
2021-09-01 02:38:04 +0900

    Add SetCompanyActivityTest

--- app/src/androidTest/java/com/example/soundlesscheck_in/SetCompanyActivityTest.java
+++ app/src/androidTest/java/com/example/soundlesscheck_in/SetCompanyActivityTest.java
@@ -1,8 +1,13 @@
 package com.example.soundlesscheck_in;
 
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 import androidx.test.espresso.Espresso;
@@ -10,6 +15,7 @@ import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,26 +28,44 @@ public class SetCompanyActivityTest {
 
     @Test
     public void testOkBtn() {
-        Espresso.onView(ViewMatchers.withId(R.id.btnGetCompanyInfo)).perform(click());
+        onView(withId(R.id.btnGetCompanyInfo)).perform(click());
     }
 
     @Test
     public void testCancleBtn() {
-        Espresso.onView(ViewMatchers.withId(R.id.btnCancleCompanyInfo)).perform(click());
+        onView(withId(R.id.btnCancleCompanyInfo)).perform(click());
     }
 
     @Test
     public void testTextViewNum() {
-        Espresso.onView(ViewMatchers.withId(R.id.companyNumTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.companyNumTextView)).check(matches(isDisplayed()));
     }
 
     @Test
     public void testTextViewName() {
-        Espresso.onView(ViewMatchers.withId(R.id.companyNameTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.companyNameTextView)).check(matches(isDisplayed()));
     }
 
     @Test
     public void testTextViewLoc() {
-        Espresso.onView(ViewMatchers.withId(R.id.companyLocTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.companyLocTextView)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testEditTextNum() {
+        onView(withId(R.id.companyNumEditText))
+                .perform(typeText("123-45-67890"), closeSoftKeyboard());
+
+        onView(withId(R.id.companyNumEditText))
+                .check(matches(withText("123-45-67890")));
+    }
+
+    @Test
+    public void testEditTextName() {
+        onView(withId(R.id.companyNameEditText))
+                .perform(typeText("Starbucks"), closeSoftKeyboard());
+
+        onView(withId(R.id.companyNameEditText))
+                .check(matches(withText("Starbucks")));
     }
 }
